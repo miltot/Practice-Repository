ae69eb7fbdaf17abe28f5d22a7d5b9d3b519929d
milto tasissa <miltot1000@gmail.com>
2020-10-26 10:35:14 -0400

    code prints hello world

--- practice_git_code.py
+++ practice_git_code.py
@@ -1,25 +1,3 @@
-"""simple social network for tracking connectins between people  """
-
-
-class Person:
-    """a person in a social network
-    Attributes: 
-    
-    name(str): the name of the person 
-    connections (set of person) : other people on the social network who know this person
-    
-    
-    
-    
-    """
-    
-    def __init__(self,name):
-        """initialize a new person object"""
-        self.name = name 
-        self.connections = set()
-        
-    def connect(self,person2):
-        """connect with person 2"""
-        
+print ("hello world")
         
     
