ff83a5a5f03a43470c85805f686c85b020b7b3c8
milto tasissa <miltot1000@gmail.com>
2020-10-26 08:16:33 -0400

    added practice_git_code.py

---
+++ practice_git_code.py
@@ -0,0 +1,16 @@
+"""simple social network for tracking connectins between people  """
+
+
+class Person:
+    """a person in a social network
+    Attributes: 
+    
+    name(str): the name of the person 
+    connections (set of person) : other people on the social network who know this person
+    
+    
+    
+    
+    """
+    
+    
